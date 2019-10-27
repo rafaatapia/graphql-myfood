2e678c9774a51464936b11262deb446db99c5c15
Rafael Tapia <rafaelsilvatapia@gmail.com>
2019-10-26 22:41:35 -0300

    Ajustes para corrigir os erros eslint

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,36 +1,36 @@
-import { GraphQLServer } from "graphql-yoga";
-import { resolve } from "path";
+import { GraphQLServer } from 'graphql-yoga'
+import { resolve } from 'path'
 
-const typeDefs = resolve(__dirname, "schema.graphql");
+const typeDefs = resolve(__dirname, 'schema.graphql')
 const USERS = [
-  { id: 1, name: "Tony Stark", email: "stark@avengers.com" },
-  { id: 2, name: "Spider Man", email: "spider@avengers.com" }
-];
+  { id: 1, name: 'Tony Stark', email: 'stark@avengers.com' },
+  { id: 2, name: 'Spider Man', email: 'spider@avengers.com' }
+]
 
 const resolvers = {
   User: {
-    name: parent => {
-      console.log("Parent: ", parent);
-      return "User: " + parent.name;
+    name: (parent): string => {
+      console.log('Parent: ', parent)
+      return 'User: ' + parent.name
     }
   },
   Query: {
-    users: () => USERS
+    users: (): typeof USERS => USERS
   },
   Mutation: {
-    createUser: (parent, args, ctx, info) => {
-      console.log("args: ", args);
-      const { data } = args;
+    createUser: (args): typeof USERS[0] => {
+      console.log('args: ', args)
+      const { data } = args
       const user = {
         ...data,
         id: USERS.length + 1
-      };
-      USERS.push(user);
-      return user;
+      }
+      USERS.push(user)
+      return user
     }
   }
-};
+}
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ typeDefs, resolvers })
 
-export default server;
+export default server
